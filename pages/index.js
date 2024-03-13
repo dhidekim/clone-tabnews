c5f05e818b7ebab5df819401ac104b4f84b3a5e2
Douglas Hideki Matsudo <dhidekim@gmail.com>
2024-03-13 20:19:36 +0000

    atualizando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Muito obrigado por tudo, saiba que ano você e nossa linda família!</h1>
+    return <h1>Greicy! Muito obrigado por tudo, saiba que ano você e nossa linda família!</h1>
 }
 
 export default Home;
