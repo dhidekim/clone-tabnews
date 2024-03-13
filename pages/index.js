a523604ff93fbd40e27cd62861b9cad6bf60f134
Douglas Hideki Matsudo <dhidekim@gmail.com>
2024-03-13 20:36:22 +0000

    adicionando emoticon

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Greicy! Muito obrigado por tudo, saiba que amo você e nossa linda família!</h1>
+    return <h1>Greicy! Muito obrigado por tudo, saiba que amo você e nossa linda família! 😍</h1>
 }
 
 export default Home;
