f47e0fee6b2cbb8250a56a02c5db5489719ffc41
Douglas Hideki Matsudo <dhidekim@gmail.com>
2024-03-16 03:17:27 +0000

    adiciona script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para acompanhamento do curso.dev de Filipe Deschamps.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-function Home(){
-    return <h1>Greicy! Muito obrigado por tudo, saiba que amo você e nossa linda família! 😍</h1>
+function Home() {
+  return <h1>Greicy! Muito obrigado por tudo, saiba que amo você e nossa linda família!😍</h1>;
 }
 
 export default Home;
